01fa7ec055ccdc2b813477213e151e8ef1404c2a
heyitszinx-ongit <80139255+heyitszinx-ongit@users.noreply.github.com>
2021-06-02 11:09:02 +0100

    Source Code

---
+++ Astroid/Injector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Fortnite
+{
+	// Token: 0x02000002 RID: 2
+	internal class Injector
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002048 File Offset: 0x00000248
+		public static void Inject(int processId, string path)
+		{
+			IntPtr hProcess = Injector.OpenProcess(1082, false, processId);
+			IntPtr procAddress = Injector.GetProcAddress(Injector.GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+			uint num = (uint)((path.Length + 1) * Marshal.SizeOf(typeof(char)));
+			IntPtr intPtr = Injector.VirtualAllocEx(hProcess, IntPtr.Zero, num, 12288U, 4U);
+			UIntPtr uintPtr;
+			Injector.WriteProcessMemory(hProcess, intPtr, Encoding.Default.GetBytes(path), num, out uintPtr);
+			Injector.CreateRemoteThread(hProcess, IntPtr.Zero, 0U, procAddress, intPtr, 0U, IntPtr.Zero);
+		}
+
+		// Token: 0x06000002 RID: 2
+		[DllImport("kernel32.dll")]
+		public static extern int SuspendThread(IntPtr hThread);
+
+		// Token: 0x06000003 RID: 3
+		[DllImport("kernel32.dll")]
+		public static extern int ResumeThread(IntPtr hThread);
+
+		// Token: 0x06000004 RID: 4
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr OpenThread(int dwDesiredAccess, bool bInheritHandle, int dwThreadId);
+
+		// Token: 0x06000005 RID: 5
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool CloseHandle(IntPtr hHandle);
+
+		// Token: 0x06000006 RID: 6
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+		// Token: 0x06000007 RID: 7
+		[DllImport("kernel32", CharSet = CharSet.Ansi, SetLastError = true)]
+		public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+		// Token: 0x06000008 RID: 8
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+		// Token: 0x06000009 RID: 9
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+		// Token: 0x0600000A RID: 10
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+		// Token: 0x0600000B RID: 11
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+	}
+}
---
+++ Astroid/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Fortnite
+{
+	// Token: 0x02000003 RID: 3
+	internal class Program
+	{
+		// Token: 0x0600000D RID: 13 RVA: 0x000020D8 File Offset: 0x000002D8
+		private static void Main(string[] args)
+		{
+			Console.Title = "CometFN";
+			string answer;
+
+			Console.Write("Would you like to launch Astroid now?\n");
+			answer = Console.ReadLine();
+			if (answer.Equals("yes", StringComparison.InvariantCultureIgnoreCase))
+			{
+				Console.Clear();
+				string str = string.Empty;
+				string text = string.Empty;
+				foreach (string text2 in args)
+				{
+					bool flag = text2.Contains("-epicusername=");
+					bool flag2 = flag;
+					bool flag3 = flag2;
+					if (flag3)
+					{
+						str = text2.Replace("-epicusername=", "");
+					}
+					text = text + text2 + " ";
+				}
+				Console.WriteLine("Welcome to Astroid.");
+				Thread.Sleep(1000);
+				Console.WriteLine("Credits to HeyItsZinx and bolt!");
+				Thread.Sleep(1000);
+				Console.WriteLine("Launching...");
+				Thread.Sleep(2000);
+				Process.Start("FortniteClient-Win64-Shipping.exe", text);
+				Process[] processes = Process.GetProcesses();
+				foreach (Process process in processes)
+				{
+					bool flag4 = process.ProcessName.Contains("FortniteClient-Win64-Shipping");
+					bool flag5 = flag4;
+					bool flag6 = flag5;
+					if (flag6)
+					{
+						Console.WriteLine("Fortnite Launched!");
+						break;
+					}
+				}
+				Console.Read();
+			}
+			else if (answer.Equals("no", StringComparison.InvariantCultureIgnoreCase))
+			{
+				Console.WriteLine("Okay.");
+				Thread.Sleep(1000);
+				Environment.Exit(0);
+			}
+		}
+	}
+}
